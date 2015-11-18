77be2c8b2f81ba10d483c31d84122d3fa8793e15
andrechambers <andre@evanschambers.com>
2015-11-17 18:40:28 -0800

    adding new book method

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -4,4 +4,20 @@ class BooksController < ApplicationController
     @available_at =Time.now
     @books = ["Atlas Shrugged", "Ender's Game", "Hamlet", "The Hobbit"]
   end
+
+  def new
+    @book = Book.new
+  end
+
+  def create
+    @book = Book.new(book_params)
+    @book.save
+    redirect_to @book
+  end
+
+  private
+
+  def book_params
+    params.require(:book).permit(:title, :author, :pages, :price)
+
 end
