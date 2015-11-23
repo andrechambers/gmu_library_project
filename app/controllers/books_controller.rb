c0df75174473608b4f0f9d30a7241e5bd5d3280b
andrechambers <andre@evanschambers.com>
2015-11-22 18:06:32 -0800

    update to pull all books from database

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -2,7 +2,8 @@ class BooksController < ApplicationController
   def index
 
     @available_at =Time.now
-    @books = ["Atlas Shrugged", "Ender's Game", "Hamlet", "The Hobbit"]
+    @books = Book.all
+    # ["Atlas Shrugged", "Ender's Game", "Hamlet", "The Hobbit"]
   end
 
   def new
@@ -13,6 +14,20 @@ class BooksController < ApplicationController
     @book = Book.new(book_params)
     @book.save
     redirect_to @book
+  end
+
+  def show
+    @book = Book.find(params[:id])
+  end
+
+  def edit
+    @book = Book.find(params[:id])
+  end
+
+  def update
+    @book = Book.find(params[:id])
+    @book.update(books_params)
+    redirect to @book
   end
 
   private
